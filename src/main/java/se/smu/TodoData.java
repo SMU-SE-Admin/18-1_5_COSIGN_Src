661dcd4db3c1199c05353bb49fa7f1df2e6d0371
201611043 <perryclass73@naver.com>
2018-05-18 16:57:53 +0900

    #1117 To Do 내용 편집 마감일 설정

---
+++ src/main/java/se/smu/TodoData.java
@@ -0,0 +1,16 @@
+package se.smu;
+
+public class TodoData {
+
+	// Todo 객체. 마감일 입력받는 파트. 수정 - 차인협	
+	int Deadline_Year;
+	int Deadline_Month;
+	int Deadline_Date;
+	
+	public TodoData ( int arg1, int arg2, int arg3 ){
+		Deadline_Year = arg1;
+		Deadline_Month = arg2;
+		Deadline_Date = arg3;
+	}
+
+}
