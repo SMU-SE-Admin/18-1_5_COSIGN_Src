c23a26df96bb66bffb618c1ce1a75df5d125a61c
201611043 <perryclass73@naver.com>
2018-05-18 17:00:41 +0900

    #1120 알람 D-day 설정 불러오기

---
+++ src/main/java/se/smu/CallDeadLine.java
@@ -0,0 +1,17 @@
+package se.smu;
+
+//특정 Todo의 마감일을 호출해오는 객체
+public class CallDeadLine {
+
+	int Deadline_Year;
+	int Deadline_Month;
+	int Deadline_Date;
+	
+	public CallDeadLine (int arg1, int arg2, int arg3 ){
+		Deadline_Year = arg1;
+		Deadline_Month = arg2;
+		Deadline_Date = arg3;
+	}
+
+	
+}
